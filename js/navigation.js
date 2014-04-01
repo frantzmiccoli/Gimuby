1388fcfe299855b468e6f872e4246b3853e8b041
Fräntz Miccoli <frantz.miccoli@fakedomain.com>
2014-04-02 00:09:00 +0200

    Hash autoupdate

--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,14 @@
 ;(function($) {
     var Navigation = function() {
 
-        self._init = function() {
-            self._bindListeners();
+        this._init = function() {
+            this._bindListeners();
+
+            this._scrollEventStackSize = 0;
+            this._scrollHandlingDelay = 300;
         };
 
-        self._bindListeners = function() {
+        this._bindListeners = function() {
             var objectRef = this;
             $('a').each(function() {
                 var $element = $(this),
@@ -15,13 +18,17 @@
                     var targetAnchor = href.replace('#', '');
                     $element.bind('click', function() {
                         return objectRef._onClick(targetAnchor);
-                    })
+                    });
                 }
 
             });
+
+            $(window).bind('scroll', function() {
+                objectRef._onScroll();
+            });
         };
 
-        self._onClick = function(targetAnchor) {
+        this._onClick = function(targetAnchor) {
             var $targetElement = $('#' + targetAnchor);
             $('html,body').animate(
                 {scrollTop: $targetElement.offset().top},
@@ -33,7 +40,53 @@
             return false;
         };
 
-        self._init();
+        this._onScroll = function() {
+            var objectRef = this;
+            this._scrollEventStackSize += 1;
+            setTimeout(function() {
+                objectRef._scrollEventStackSize -= 1;
+                if (objectRef._scrollEventStackSize === 0) {
+                    objectRef._updateHashFromScroll();
+                }
+            }, objectRef._scrollHandlingDelay);
+        };
+
+        this._updateHashFromScroll = function() {
+            var currentScroll = window.pageYOffset + 200,
+                $closestBlock = this._getClosestUpperBlock(currentScroll);
+
+            if (typeof($closestBlock) === 'undefined') {
+                window.location.hash = '';
+                return;
+            }
+
+            var blockId = $closestBlock.attr('id');
+            $closestBlock.attr('id', '');
+            window.location.hash = blockId;
+            $closestBlock.attr('id', blockId);
+        };
+
+        this._getClosestUpperBlock = function(currentScroll) {
+            var $closestBlock = undefined,
+                closestOffsetDelta = undefined;
+
+            $('.content-block').each(function() {
+                var $element = $(this),
+                    currentOffset = $element.offset().top,
+                    currentDelta = currentScroll - currentOffset;
+
+                if ((currentDelta > 0) &&
+                    ((typeof($closestBlock) === 'undefined')
+                        || (currentDelta < closestOffsetDelta))) {
+                    $closestBlock = $element;
+                    closestOffsetDelta = currentDelta;
+                }
+            });
+
+            return $closestBlock;
+        };
+
+        this._init();
     };
 
     $(document).ready(function() {
